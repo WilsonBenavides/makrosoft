408b17706f79ff6bb6beffb32f0ef45626a865ec
willix <benavidesb@unicauca.edu.co>
2022-10-10 09:26:31 -0500

    30 formularios clase AbstractType

--- src/AppBundle/Controller/GestionTapasController.php
+++ src/AppBundle/Controller/GestionTapasController.php
@@ -6,10 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Tapa;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use AppBundle\Form\TapaType;
 
 /**
      * @Route("/gestionTapas")
@@ -23,12 +20,7 @@ class GestionTapasController extends Controller
     {
         $tapa = new Tapa();
         //contruyendo el formulario
-        $formBuilder = $this->createFormBuilder( $tapa );
-        $formBuilder->add( 'nombre', TextType::class );
-        $formBuilder->add( 'descripcion', TextareaType::class );
-        $formBuilder->add( 'fechaCreacion', DateType::class );
-        $formBuilder->add( 'salvar', SubmitType::class, array( 'label' => 'Nueva Tapa' ));
-        $form = $formBuilder->getForm();
+        $form = $this->createForm( TapaType::class, $tapa );    
         
         return $this->render('gestionTapas/nuevaTapa.html.twig', array( 'form' => $form->createView() ));
     }
